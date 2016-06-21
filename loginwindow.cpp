f504217282a99eda1fd5e2191c2232a9d1214923
Kyle M Hall <kyle@kylehall.info>
2016-06-21 09:25:25 -0400

    Allow logo to be replaced with a URL
    Test use, set a section in your Libki client ini as such:
    [images]
    logo="http://path/to/logo.png"
    logo_height="500"
    logo_width="400"
    
    The height and width are optional, but will probably be necessary if you
    want your logo centered perfectly. The logo is actually a web view so you
    would in theory load an html file instead!

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -80,6 +80,31 @@ void LoginWindow::getSettings() {
         passwordLabel->setText(settings.value("labels/password").toString() );
     }
 
+    if ( ! settings.value("images/logo").toString().isEmpty() ) {
+        logo->hide();
+
+        QPalette palette = logoWebView->palette();
+        palette.setBrush(QPalette::Base, Qt::transparent);
+
+        QString logoUrl = settings.value("images/logo").toString();
+        qDebug() << "Logo URL: " << logoUrl;
+
+        if ( ! logoUrl.isEmpty() ) {
+            int logoWidth = settings.value("images/logo_width").toInt();
+            if ( logoWidth ) { logoWebView->setMaximumWidth(logoWidth); }
+
+            int logoHeight = settings.value("images/logo_height").toInt();
+            if ( logoHeight ) { logoWebView->setMaximumHeight(logoHeight); }
+
+            logoWebView->setEnabled(true);
+            logoWebView->page()->setPalette(palette);
+            logoWebView->setAttribute(Qt::WA_OpaquePaintEvent, false);
+            logoWebView->load(QUrl(logoUrl));
+        }
+    } else {
+        logoWebView->hide();
+    }
+
     /* Hide Password Field */
 
     if ( settings.value("node/no_passwords").toString().toInt() ) {
