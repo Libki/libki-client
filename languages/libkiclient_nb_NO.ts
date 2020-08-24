ac96c775fc72da475f0c80be94976af09e7a2303
Kyle M Hall <kyle@bywatersolutions.com>
2020-08-24 08:47:02 -0400

    Add missing renamed NO translation files

---
+++ languages/libkiclient_nb_NO.ts
@@ -0,0 +1,240 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="../loginwindow.ui" line="14"/>
+        <source>Libki Kiosk System</source>
+        <translation>Libki innloggingssystem</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="56"/>
+        <source>Internet Kiosk</source>
+        <translation>PublikumsPC</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="110"/>
+        <location filename="../loginwindow.cpp" line="258"/>
+        <source>Reserved</source>
+        <translation>Reservert</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="140"/>
+        <source>Please Log In.</source>
+        <translation>Vennligst logg inn.</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="196"/>
+        <source>Username:</source>
+        <translation>Brukernavn:</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="212"/>
+        <source>Password:</source>
+        <translation>Passord:</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="232"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="243"/>
+        <source>Log In</source>
+        <translation>Logg inn</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="342"/>
+        <location filename="../loginwindow.ui" line="408"/>
+        <source>about:blank</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="100"/>
+        <source>Please Wait...</source>
+        <translation>Vent litt ...</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="174"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="175"/>
+        <source>No</source>
+        <translation>Nei</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="132"/>
+        <source>Login Failed: Username and password do not match</source>
+        <translation>Feil brukernavn eller passord</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="134"/>
+        <source>Login Failed: No time left</source>
+        <translation>Du har brukt opp tildelt tid på PCen</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="136"/>
+        <source>Login Failed: Account is currently in use</source>
+        <translation>Brukerkontoen er i bruk fra før</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="138"/>
+        <source>Login Failed: Account is disabled</source>
+        <translation>Brukeskontoen er sperret</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="140"/>
+        <source>Login Failed: This kiosk is reserved for someone else</source>
+        <translation>PCen er reservert en annen bruker</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="142"/>
+        <source>Login Failed: Reservation required</source>
+        <translation>Du må reservere tid for å bruke PCen</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="144"/>
+        <location filename="../loginwindow.cpp" line="168"/>
+        <source>Login Failed: You have excessive outstanding fees</source>
+        <translation>Du har for store bøter</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="146"/>
+        <source>Login Failed: Charge privileges denied</source>
+        <translation>Utvidet bruk er avslått</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="148"/>
+        <source>Login Failed: Renewal privileges denied</source>
+        <translation>Fornyet bruk er avslått</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="150"/>
+        <source>Login Failed: Recall privileges denied</source>
+        <translation>Gjenopptatt bruk er avslått</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="152"/>
+        <source>Login Failed: Hold privileges denied</source>
+        <translation>Pause i bruk av PC er avslått</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="154"/>
+        <source>Login Failed: Your card has been reported lost</source>
+        <translation>Kortet ditt kort er meldt mistet</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="156"/>
+        <source>Login Failed: You have too many items charged to your account</source>
+        <translation>Du har for mange lån på kontoen din</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="158"/>
+        <source>Login Failed: You have too many items overdue</source>
+        <translation>Du har for mange lån som har utløpt på tid</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="160"/>
+        <source>Login Failed: You have renewed items too many times</source>
+        <translation>Du har fornytet dine lån for mange ganger</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="162"/>
+        <source>Login Failed: You have claimed to many items as returned</source>
+        <translation>Du har påstått at du har levert tilbake for mange utlån</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="164"/>
+        <source>Login Failed: You have lost too many items</source>
+        <translation>Du har mistet for mange utlån</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="166"/>
+        <source>Login Failed: You have excessive outstanding fines</source>
+        <translation>Du har for store bøter</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="170"/>
+        <source>Login Failed: You have a recalled item which is overdue</source>
+        <translation>Du har et utløpt utlån</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="172"/>
+        <source>Login Failed: You have been billed for too many items</source>
+        <translation>Du har for mange utløpte avgifter</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="174"/>
+        <source>Login Failed: Client not registered</source>
+        <translation>PCen er ikke slått av</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="176"/>
+        <source>Login Failed: Unable to connect to ILS</source>
+        <translation>Klarte ikke å koble til bibliotekssystemet</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="178"/>
+        <source>Login Failed: Too many concurrent sessions on this account</source>
+        <translation>For mange pågående innlogginger på denne kontoen</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="178"/>
+        <source>Login Failed: </source>
+        <translation>Innlogging feilet: </translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="256"/>
+        <source>Reserved for </source>
+        <translation>Reservert for </translation>
+    </message>
+</context>
+<context>
+    <name>TimerWindow</name>
+    <message>
+        <location filename="../timerwindow.ui" line="14"/>
+        <source>Libki Kiosk System</source>
+        <translation>Libki datorbokningssystem</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.ui" line="98"/>
+        <location filename="../timerwindow.cpp" line="139"/>
+        <source>Log Out</source>
+        <translation>Logg ut</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.ui" line="131"/>
+        <source>Lock System</source>
+        <translation>Lås PCen</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="92"/>
+        <location filename="../timerwindow.cpp" line="184"/>
+        <source>Minutes Left</source>
+        <translation>minutter igjen</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="112"/>
+        <source>Log Out?</source>
+        <translation>Logge ut?</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="113"/>
+        <source>Are you sure you want to log out?</source>
+        <translation>Er du sikker på at du vil logge ut?</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="183"/>
+        <source>Time Remaining</source>
+        <translation>Tid igjen</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="199"/>
+        <source>You Have Recieve A Message</source>
+        <translation>Du har en beskjed</translation>
+    </message>
+</context>
+</TS>
