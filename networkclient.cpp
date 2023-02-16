1980f16de1216a7dd9562ee6e37afd16ae41db81
Kyle M Hall <kyle@bywatersolutions.com>
2023-02-16 11:55:14 -0500

    Bump client version to 2.2.19

--- networkclient.cpp
+++ networkclient.cpp
@@ -476,7 +476,7 @@ void NetworkClient::registerNode() {
   query.addQueryItem("action", "register_node");
   query.addQueryItem("node_name", nodeName);
   query.addQueryItem("age_limit", nodeAgeLimit);
-  query.addQueryItem("version", "2.2.18");
+  query.addQueryItem("version", "2.2.19");
   url.setQuery(query);
 
   /*QNetworkReply* reply =*/nam->get(QNetworkRequest(url));
