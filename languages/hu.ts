0d779e94e29ba5adc908d0c7ea1ef6673287b969
kkalmanigyuk <136581882+kkalmanigyuk@users.noreply.github.com>
2025-11-21 09:47:16 +0100

    Create hu.ts
    .ts file for Hungarian client translation

---
+++ languages/hu.ts
@@ -0,0 +1,376 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="../loginwindow.ui" line="14"/>
+        <source>Libki Kiosk System</source>
+        <translation>Libki Számítógép-kezelő rendszer</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="30"/>
+        <source>Internet Kiosk</source>
+        <translation>Olvasói számítógép</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="59"/>
+        <location filename="../loginwindow.ui" line="398"/>
+        <location filename="../loginwindow.ui" line="464"/>
+        <source>about:blank</source>
+        <translation>about:blank</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="126"/>
+        <location filename="../loginwindow.cpp" line="506"/>
+        <source>Reserved</source>
+        <translation>Foglalt</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="156"/>
+        <source>Please Log In.</source>
+        <translation>Jelentkezzen be 
+(Regisztrációs kártya aktiválásáért forduljon a könyvtároshoz)</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="230"/>
+        <source>Username:</source>
+        <translation>Olvasójegyszám:</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="246"/>
+        <source>Password:</source>
+        <translation>Jelszó:</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="266"/>
+        <source>Cancel</source>
+        <translation>Mégsem</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="282"/>
+        <source>Log In</source>
+        <translation>Bejelentkezés</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="302"/>
+        <source>Guest Log In</source>
+        <translation>Vendég bejelentkezés</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="531"/>
+        <source>2.2.32</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="579"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;Unable to connect to server&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Hiba a szerverhez való csatlakozáskor</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.ui" line="603"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;Unable to connect to Internet&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Hiba az internetkapcsolatban</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="165"/>
+        <location filename="../loginwindow.cpp" line="217"/>
+        <source>Please Wait...</source>
+        <translation>Kérem várjon...</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="175"/>
+        <location filename="../loginwindow.cpp" line="256"/>
+        <source>Do you accept the terms of service?</source>
+        <translation>Elfogadja a szolgáltatlási feltételeket?</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="180"/>
+        <location filename="../loginwindow.cpp" line="261"/>
+        <source>Terms of Service</source>
+        <translation>Szolgáltatási feltételek</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="194"/>
+        <location filename="../loginwindow.cpp" line="275"/>
+        <source>Yes</source>
+        <translation>Igen</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="195"/>
+        <location filename="../loginwindow.cpp" line="276"/>
+        <source>No</source>
+        <translation>Nem</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="297"/>
+        <source>Login Failed: Username and password do not match</source>
+        <translation>Érvénytelen felhasználó / Lejárt olvasójegy</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="300"/>
+        <source>Login Failed: You are not the correct age to use this client</source>
+        <translation>Bejelentkezés: Felhasználó életkora nem megfelelő</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="302"/>
+        <source>Login Failed: No time left</source>
+        <translation>Bejelentkezés: Időkeret elfogyott</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="304"/>
+        <source>Login Failed: This kiosk is closed for the day</source>
+        <translation>Bejelentkezés: A számítógép ma már nem elérhető</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="306"/>
+        <source>Login Failed: Account is currently in use</source>
+        <translation>Bejelentkezés: A felhasználói fiók már használatban</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="308"/>
+        <source>Login Failed: Account is disabled</source>
+        <translation>Bejelentkezés: Felhasználó letiltva
+Regisztrációs kártya esetén forduljon a könyvtároshoz!</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="311"/>
+        <source>Login Failed: This kiosk is reserved for someone else</source>
+        <translation>Bejelentkezés: A számítógépre más felhasználó foglalása él</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="313"/>
+        <source>Login Failed: Reservation required</source>
+        <translation>Hiba: Foglalás szükséges</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="316"/>
+        <location filename="../loginwindow.cpp" line="345"/>
+        <source>Login Failed: You have excessive outstanding fees</source>
+        <translation>Bejelentkezés: Befizetetlen hátraléka van</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="318"/>
+        <source>Login Failed: Charge privileges denied</source>
+        <translation>f</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="320"/>
+        <source>Login Failed: Renewal privileges denied</source>
+        <translation>Bejelentkezés: Hosszabbítási jogosultsága megtagadva</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="322"/>
+        <source>Login Failed: Recall privileges denied</source>
+        <translation>Bejelentkezés: Visszahívási jogosultságát megtagadták</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="324"/>
+        <source>Login Failed: Hold privileges denied</source>
+        <translation>Bejelentkezés: Előjegyzési jogosultságát megtagadták</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="326"/>
+        <source>Login Failed: Your card has been reported lost</source>
+        <translation>Bejelentkezés: Olvasójegyét elveszettként jelentették</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="329"/>
+        <source>Login Failed: You have too many items charged to your account</source>
+        <translation>Bejelentkezés: Túl sok tétel szerepel az Ön nevén</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="331"/>
+        <source>Login Failed: You have too many items overdue</source>
+        <translation>Bejelentkezés: Túl sok lejárt dokumentum szerepel az Ön nevén</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="334"/>
+        <source>Login Failed: You have renewed items too many times</source>
+        <translation>Bejelentkezés: Túl sok dokumentumot hosszabbított meg</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="337"/>
+        <source>Login Failed: You have claimed too many items as returned</source>
+        <translation>Bejelentkezés: Túl sok dokumentumot jelentett visszahozottnak</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="339"/>
+        <source>Login Failed: You have have lost too many items</source>
+        <translation>Bejelentkezés: Túl sok dokumentumot vesztett el</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="342"/>
+        <source>Login Failed: You have excessive outstanding fines</source>
+        <translation>Bejelentkezés: Fizetetlen bírság van a nevén</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="348"/>
+        <source>Login Failed: You have a recalled item which is overdue</source>
+        <translation>Bejelentkezés: Lejárt dokumentum van Önnél, amelyre más igényt tart</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="351"/>
+        <source>Login Failed: You have been billed for too many items</source>
+        <translation>Bejelentkezés: Túl sok korábban elvesztett dokumentum van az Ön nevén</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="353"/>
+        <source>Login Failed: Client not registered</source>
+        <translation>Bejelentkezés: Számítógép nincs regisztrálva</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="355"/>
+        <source>Login Failed: Unable to connect to ILS</source>
+        <translation>Bejelentkezés: Sikertelen IKR csatlakozás</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="358"/>
+        <source>Login Failed: Too many concurrent sessions on this account</source>
+        <translation>Bejelentkezés: Túl sok egyidejű munkamenet él a felhasználón</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="361"/>
+        <source>Login Failed: Expired Membership. Please inquire at the circulation desk.</source>
+        <translation>Bejelentkezés: Lejárt olvasójegy. Kérjük, újítsa meg tagságát.</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="364"/>
+        <source>Login Failed: </source>
+        <translation>Bejelentkezés sikertelen: </translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="504"/>
+        <source>Reserved: </source>
+        <translation>Foglalt: </translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="603"/>
+        <source>This kiosk is out of order.</source>
+        <translation>A számítógép üzemen kívül van.</translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="621"/>
+        <source>Error connecting to server. Verify Libki server is accessible from this network. Error Code: </source>
+        <translation>Csatlakozás a szerverhez sikertelen. Győződjön meg róla, hogy a Libki szerver elérhető ezen a hálózaton. Hibakód: </translation>
+    </message>
+    <message>
+        <location filename="../loginwindow.cpp" line="634"/>
+        <source>Error connecting to Internet: </source>
+        <translation>Csatlakozás az internetre sikertelen: </translation>
+    </message>
+</context>
+<context>
+    <name>SessionLockedWindow</name>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="14"/>
+        <source>Libki Kiosk System</source>
+        <translation>Libki Számítógép-kezelő rendszer</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="56"/>
+        <source>Internet Kiosk</source>
+        <translation>Olvasói Kioszk</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="85"/>
+        <source>about:blank</source>
+        <translation>about:blank</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="152"/>
+        <source>Session Locked</source>
+        <translation>Géphasználat zárolva</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="182"/>
+        <source>Enter Password To Resume Session</source>
+        <translation>Írja be jaszlavát a gép  további használatához</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="256"/>
+        <source>Password:</source>
+        <translation>Jelszó:</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.ui" line="281"/>
+        <source>Resume</source>
+        <translation>Folytatás</translation>
+    </message>
+    <message>
+        <location filename="../sessionlockedwindow.cpp" line="129"/>
+        <source>Incorrect Password</source>
+        <translation>Helytelen jelszó</translation>
+    </message>
+</context>
+<context>
+    <name>TimerWindow</name>
+    <message>
+        <location filename="../timerwindow.ui" line="14"/>
+        <source>Libki Kiosk System</source>
+        <translation>Libki Számítógép</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.ui" line="107"/>
+        <location filename="../timerwindow.cpp" line="295"/>
+        <source>Log Out</source>
+        <translation>Kijelentkezés</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.ui" line="146"/>
+        <source>Lock Session</source>
+        <translation>Zárolás</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="120"/>
+        <source>You have one or more items on hold waiting for pickup. Please contact a librarian for more details</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="179"/>
+        <location filename="../timerwindow.cpp" line="373"/>
+        <source>Minutes Left</source>
+        <translation>Perc maradt</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="258"/>
+        <source>Log Out?</source>
+        <translation>Kijelentkezik?</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="259"/>
+        <source>Are you sure you want to log out?</source>
+        <translation>Biztosan kijelentkezik?</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="262"/>
+        <source>Yes</source>
+        <translation>Igen</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="263"/>
+        <source>Cancel</source>
+        <translation>Mégsem</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="371"/>
+        <source>Time Remaining</source>
+        <translation>Időkeret:</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="473"/>
+        <source>Inactivity detected</source>
+        <translation>Inakativitás észlelve</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="474"/>
+        <source>Please confirm you are still using this computer.</source>
+        <translation>Használja még a számítógépet?</translation>
+    </message>
+    <message>
+        <location filename="../timerwindow.cpp" line="509"/>
+        <source>You have a message</source>
+        <translation>Üzenete érkezett</translation>
+    </message>
+</context>
+</TS>
